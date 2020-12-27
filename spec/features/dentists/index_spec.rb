6bf70bfa3c1007a7afed32215453d444be258942
Sam Yeo <yeosamuel95@gmail.com>
2020-12-27 08:47:46 -0700

    Complete tests for basic content, Story 2

--- spec/features/dentists/index_spec.rb
+++ spec/features/dentists/index_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'Root Welcome Page' do
+RSpec.describe 'Dentists Index  Page' do
   before :each do
     @office1 = create(:dental_office)
     @dentist1 = create(:dentist, dental_office: @office1)
@@ -8,7 +8,7 @@ RSpec.describe 'Root Welcome Page' do
     visit '/dentists'
   end
   describe 'Content' do
-    it 'should show all dentists name as links' do
+    it 'should show all dentists names as links' do
       expect(page).to have_content(@dentist1.name)
       expect(page).to have_content(@dentist2.name)
     end
