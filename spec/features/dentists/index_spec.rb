e1c874fc132d9741721b098ff1f46dc0bf72b97b
Sam Yeo <yeosamuel95@gmail.com>
2020-12-27 08:43:31 -0700

    Create basic content test for dentists index

---
+++ spec/features/dentists/index_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe 'Root Welcome Page' do
+  before :each do
+    @office1 = create(:dental_office)
+    @dentist1 = create(:dentist, dental_office: @office1)
+    @dentist2 = create(:dentist, dental_office: @office1)
+    visit '/dentists'
+  end
+  describe 'Content' do
+    it 'should show all dentists name as links' do
+      expect(page).to have_content(@dentist1.name)
+      expect(page).to have_content(@dentist2.name)
+    end
+  end
+end
